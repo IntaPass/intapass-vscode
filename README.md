a307d3a23b75d743787a8b58e80dfc69686aba73
FELIX CHERUIYOT <felix@intasend.com>
2024-02-25 04:22:05 +0000

    Inline complete

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 Co-pilot for high performing teams and developers.
 
 
-# How to build
+## How to build
 
     vsce package
+## Support proposed APIs
+
+> https://code.visualstudio.com/api/advanced-topics/using-proposed-api
+
+    npx @vscode/dts dev
--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,94 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Range } from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "intapass" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let genComments = vscode.commands.registerCommand('intapass.genComments', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	
+	context.subscriptions.push(vscode.commands.registerCommand('intapass.genComments', () => {
 		vscode.window.showInformationMessage('Generated comments!');
-	});
+	}));
 
-	context.subscriptions.push(genComments);
-
-	let genUnitTest = vscode.commands.registerCommand('intapass.genUnitTest', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('intapass.genUnitTest', () => {
 		vscode.window.showInformationMessage('Generated unit tests!');
-	});
+	}));
 
-	context.subscriptions.push(genUnitTest);
-
-	let genDocker = vscode.commands.registerCommand('intapass.genDocker', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('intapass.genDocker', () => {
 		vscode.window.showInformationMessage('Generated Docker!');
-	});
+	}));
 
-	context.subscriptions.push(genDocker);
-
-	let genAnsible = vscode.commands.registerCommand('intapass.genAnsible', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('intapass.genAnsible', () => {
 		vscode.window.showInformationMessage('Generated Ansible!');
-	});
+	}));
 
-	context.subscriptions.push(genAnsible);
+
+	// Autocompletion
+	const provider: vscode.InlineCompletionItemProvider = {
+		async provideInlineCompletionItems(document, position, context, token) {
+			console.log('provideInlineCompletionItems triggered');
+			const regexp = /\/\/ \[(.+?),(.+?)\)(.*?):(.*)/;
+			if (position.line <= 0) {
+				return;
+			}
+
+			const result: vscode.InlineCompletionList = {
+				items: []
+			};
+
+			let offset = 1;
+			while (offset > 0) {
+				if (position.line - offset < 0) {
+					break;
+				}
+				
+				const lineBefore = document.lineAt(position.line - offset).text;
+				const matches = lineBefore.match(regexp);
+				if (!matches) {
+					break;
+				}
+				offset++;
+
+				// const start = matches[1];
+				// const startInt = parseInt(start, 10);
+				// const end = matches[2];
+				// const endInt =
+				// 	end === '*'
+				// 		? document.lineAt(position.line).text.length
+				// 		: parseInt(end, 10);
+				// const flags = matches[3];
+				// const completeBracketPairs = flags.includes('b');
+				// const isSnippet = flags.includes('s');
+				// const text = matches[4].replace(/\\n/g, '\n');
+
+				result.items.push({
+					insertText: "Hellow bitches" , //isSnippet ? new vscode.SnippetString(text) : text,
+					range: new Range(position.line, 0, position.line, 20),//new Range(position.line, startInt, position.line, endInt),
+					filterText: "absc",
+					// completeBracketPairs,
+					command: {
+						command: 'intapass.genComments',
+						title: 'My Inline Completion Demo Command'
+					}
+				});
+			}
+			return result;
+		},
+
+		// handleDidShowCompletionItem(completionItem: vscode.InlineCompletionItem): void {
+		// 	console.log('handleDidShowCompletionItem');
+		// },
+
+		// /**
+		//  * Is called when an inline completion item was accepted partially.
+		//  * @param acceptedLength The length of the substring of the inline completion that was accepted already.
+		//  */
+		// handleDidPartiallyAcceptCompletionItem(
+		// 	completionItem: vscode.InlineCompletionItem,
+		// 	acceptedLength: number
+		// ): void {
+		// 	console.log('handleDidPartiallyAcceptCompletionItem');
+		// },
+	};
+	context.subscriptions.push(vscode.languages.registerInlineCompletionItemProvider({ pattern: '**' }, provider));
 }
 
 // This method is called when your extension is deactivated
