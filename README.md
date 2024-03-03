deb7fbce635dbb1ea41536837b6f5af6861f405b
FELIX CHERUIYOT <felix@intasend.com>
2024-03-03 05:58:26 +0000

    Update extension to provide review

--- README.md
+++ README.md
@@ -6,8 +6,3 @@ Co-pilot for high performing teams and developers.
 ## How to build
 
     vsce package
-## Support proposed APIs
-
-> https://code.visualstudio.com/api/advanced-topics/using-proposed-api
-
-    npx @vscode/dts dev
--- out/extension.js
+++ out/extension.js
@@ -22,99 +22,39 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.activate = void 0;
 const vscode = __importStar(require("vscode"));
+const axios_1 = __importDefault(require("axios"));
 function activate(context) {
-    console.log('Congratulations, your extension "intapass" is now active!');
-    const provider1 = vscode.languages.registerCompletionItemProvider('python', {
-        provideCompletionItems(document, position, token, context) {
-            // a simple completion item which inserts `Hello World!`
-            const simpleCompletion = new vscode.CompletionItem('Hello World!');
-            // a completion item that inserts its text as snippet,
-            // the `insertText`-property is a `SnippetString` which will be
-            // honored by the editor.
-            const snippetCompletion = new vscode.CompletionItem('Good part of the day');
-            snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
-            const docs = new vscode.MarkdownString("Inserts a snippet that lets you select [link](x.ts).");
-            snippetCompletion.documentation = docs;
-            docs.baseUri = vscode.Uri.parse('http://example.com/a/b/c/');
-            // a completion item that can be accepted by a commit character,
-            // the `commitCharacters`-property is set which means that the completion will
-            // be inserted and then the character will be typed.
-            const commitCharacterCompletion = new vscode.CompletionItem('console');
-            commitCharacterCompletion.commitCharacters = ['.'];
-            commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
-            // a completion item that retriggers IntelliSense when being accepted,
-            // the `command`-property is set which the editor will execute after 
-            // completion has been inserted. Also, the `insertText` is set so that 
-            // a space is inserted after `new`
-            const commandCompletion = new vscode.CompletionItem('new');
-            commandCompletion.kind = vscode.CompletionItemKind.Keyword;
-            commandCompletion.insertText = 'new ';
-            commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
-            let comment = `
-"""Transfers money to the specified account.
-
-This function initiates a transaction to send money to the account associated with the provided name. It performs necessary validations on the account details and ensures the account exists and is active before proceeding with the transaction.
-
-Parameters:
-- account (int): The account number to which money will be sent. Must be a valid and active account number.
-- name (str): The name of the account holder. This is used for additional validation to ensure the account number matches the account holder's name.
-
-Returns:
-- bool: True if the transaction was successful, False otherwise. A return value of False could indicate an invalid account number, a mismatch between the account number and the holder's name, or a failure in the transaction process.
-
-Raises:
-- ValueError: If the \`account\` or \`name\` parameters do not meet validation criteria, such as if they are empty or in an invalid format.
-- ConnectionError: If there is a problem connecting to the bank's transaction system.
-
-Note:
-- This is a simplified example for documentation purposes. Actual implementation details, such as connecting to a bank's API and handling transactions, are not covered here."""`;
-            const autoComment = new vscode.CompletionItem('comment');
-            autoComment.insertText = new vscode.SnippetString(comment);
-            const commentDocs = new vscode.MarkdownString("Generate comment");
-            autoComment.documentation = commentDocs;
-            commentDocs.baseUri = vscode.Uri.parse('http://example.com/a/b/c/');
-            // return all completion items as array
-            return [
-                simpleCompletion,
-                snippetCompletion,
-                commitCharacterCompletion,
-                commandCompletion,
-                autoComment
-            ];
+    let disposable = vscode.commands.registerCommand('extension.intapassReview', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            vscode.window.showInformationMessage('No open file to review');
+            return;
         }
+        const document = editor.document;
+        const text = document.getText();
+        axios_1.default.post('https://api.intapass.com/review', {
+            code: text,
+            lang: 'python' // Determine language dynamically based on file extension if necessary
+        })
+            .then(response => {
+            const outputChannel = vscode.window.createOutputChannel('Intapass Review');
+            outputChannel.clear();
+            outputChannel.appendLine('Review Results:');
+            outputChannel.appendLine(JSON.stringify(response.data, null, 2));
+            outputChannel.show(true);
+        })
+            .catch(error => {
+            vscode.window.showErrorMessage('Failed to review code');
+            console.error(error);
+        });
     });
-    const provider2 = vscode.languages.registerCompletionItemProvider('plaintext', {
-        provideCompletionItems(document, position) {
-            // get all text until the `position` and check if it reads `console.`
-            // and if so then complete if `log`, `warn`, and `error`
-            const linePrefix = document.lineAt(position).text.slice(0, position.character);
-            if (!linePrefix.endsWith('console.')) {
-                return undefined;
-            }
-            return [
-                new vscode.CompletionItem('log', vscode.CompletionItemKind.Method),
-                new vscode.CompletionItem('warn', vscode.CompletionItemKind.Method),
-                new vscode.CompletionItem('error', vscode.CompletionItemKind.Method),
-            ];
-        }
-    }, '.' // triggered whenever a '.' is being typed
-    );
-    context.subscriptions.push(provider1, provider2);
-    context.subscriptions.push(vscode.commands.registerCommand('intapass.genComments', () => {
-        vscode.window.showInformationMessage('Generated comments!');
-    }));
-    context.subscriptions.push(vscode.commands.registerCommand('intapass.genUnitTest', () => {
-        vscode.window.showInformationMessage('Generated unit tests!');
-    }));
-    context.subscriptions.push(vscode.commands.registerCommand('intapass.genDocker', () => {
-        vscode.window.showInformationMessage('Generated Docker!');
-    }));
-    context.subscriptions.push(vscode.commands.registerCommand('intapass.genAnsible', () => {
-        vscode.window.showInformationMessage('Generated Ansible!');
-    }));
+    context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 //# sourceMappingURL=extension.js.map
